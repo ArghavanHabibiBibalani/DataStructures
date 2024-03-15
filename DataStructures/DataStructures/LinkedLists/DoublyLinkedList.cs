4f2cecf98b5e322b5ba9a8b3c68d5330e67dce72
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-15 12:02:06 +0330

    complete class doubly linked list without testing it and interface.

--- DataStructures/DataStructures/LinkedLists/DoublyLinkedList.cs
+++ DataStructures/DataStructures/LinkedLists/DoublyLinkedList.cs
@@ -1,4 +1,6 @@
-﻿namespace DataStructures.LinkedLists
+﻿using System.Collections.Generic;
+
+namespace DataStructures.LinkedLists
 {
     internal class DoublyLinkedList<E>
     {
@@ -39,5 +41,124 @@
                 next = n;
             }
         }
+
+        private Node<E> header;
+        private Node<E> trailer;
+        private int size = 0;
+
+        public DoublyLinkedList()
+        {
+            header = new Node<E>(default(E), null, null);
+            trailer = new Node<E>(default(E), header, null);
+            header.setNext(trailer);
+        }
+
+        public int getSize()
+        {
+            return size;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+        
+        public E firstElement()
+        {
+            E first;
+
+            if (isEmpty())
+            {
+                first = default(E);
+            }
+            else
+            {
+                first = header.getNext().getElement();
+            }
+
+            return first;
+        }
+
+        public E lastElement()
+        {
+            E last ;
+
+            if (isEmpty())
+            {
+                last = default(E);
+            }
+            else
+            {
+                last = header.getPrev().getElement();
+            }
+
+            return last;
+        }
+
+        public void addFirst(E e)
+        {
+            addBetween(e, header, header.getNext());
+        }
+
+        public void addLast(E e)
+        {
+            addBetween(e, trailer.getPrev(), trailer);
+        }
+
+        public E removeFirst()
+        {
+            E result;
+            if (isEmpty())
+            {
+                result = default(E);
+            }
+            else
+            {
+                result = removeNode(header.getNext());
+            }
+
+            return result;
+
+        }
+
+        public E removeLast()
+        {
+            E result;
+            if (isEmpty())
+            {
+                result = default(E);
+            }
+            else
+            {
+                result = removeNode(trailer.getPrev());
+            }
+
+            return result;
+
+        }
+
+        private void addBetween(E e, Node<E> preducessor,  Node<E> successor)
+        {
+            Node<E> newNode = new Node<E>(e, preducessor, successor);
+
+            preducessor.setNext(newNode);
+            successor.setPrev(newNode);
+
+            size++;
+        }
+
+        private E removeNode(Node<E> node)
+        {
+            Node<E> preducessor = node.getPrev();
+            Node<E> successor = node.getNext();
+
+            preducessor.setNext(successor);
+            successor.setPrev(preducessor);
+
+            size--;
+
+            return node.getElement();
+        }
+
     }
 }
