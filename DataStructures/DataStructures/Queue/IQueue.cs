6aaa1a0dd19595aaf277064288e34f12b65494a2
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-17 17:36:52 +0330

    Queue interface.

---
+++ DataStructures/DataStructures/Queue/IQueue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Queue
+{
+    internal interface IQueue<E>
+    {
+        int Size();
+        bool IsEmpty();
+        void Enqueue(E e);
+        E Dequeue();
+        E First();
+
+    }
+}
