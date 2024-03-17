009e788a2ce6a08149efe7c670878db5acdb907b
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-17 19:29:49 +0330

    add IList interface

---
+++ DataStructures/DataStructures/Lists/IList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Lists
+{
+    internal interface IList<E>
+    {
+        int Size();
+        bool IsEmpty();
+        E Get(int index);
+        E Set(int index, E value);
+        void Add(int index, E value);
+        E Remove(int index);
+
+    }
+}
