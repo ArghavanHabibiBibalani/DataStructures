f8fb7be54ecf40a0b5c36937f574ef5d9d05e0e1
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-17 20:22:26 +0330

    add IPositionalList interface.

---
+++ DataStructures/DataStructures/Lists/PositionalLists/IPositionalList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Lists.PositionalLists
+{
+    internal interface IPositionalList<E>
+    {
+        int Size();
+        bool IsEmpty();
+        IPosition<E> First();
+        IPosition<E> Last();
+        IPosition<E> Befor(IPosition<E> position);
+        IPosition<E> After(IPosition<E> position);
+        IPosition<E> AddFirst(E element);
+        IPosition<E> AddLast(E element);
+        IPosition<E> AddAfter(IPosition<E> position, E element);
+        IPosition<E> AddBefore(IPosition<E> position, E element);
+        E Set(IPosition<E> position, E element);
+        E Remove(IPosition<E> position);
+
+    }
+}
