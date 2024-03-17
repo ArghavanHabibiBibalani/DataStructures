98d168a74427f6f0cfb02204c21de94fe26c7437
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-17 20:22:01 +0330

    add IPosition.

--- DataStructures/DataStructures/obj/Debug/net6.0/DataStructures.AssemblyInfo.cs
+++ DataStructures/DataStructures/obj/Debug/net6.0/DataStructures.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataStructures")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c4a95161dcd6a9ddfff0cc3e657be45d4ec331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataStructures")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataStructures")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
