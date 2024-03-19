a759174c5d82ad36b71f97af04e9ffac8f594be9
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-19 15:16:14 +0330

    add IBinaryTree.

---
+++ DataStructures/DataStructures/Tree/BinaryTree/IBinaryTree.cs
@@ -0,0 +1,16 @@
+﻿using DataStructures.Lists.PositionalLists;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tree.BinaryTree
+{
+    internal interface IBinaryTree<E> : ITree<E>
+    {
+        IPosition<E> Left(IPosition<E> position);
+        IPosition<E> Right(IPosition<E> position);
+        IPosition<E> Sibling(IPosition<E> position);
+    }
+}
