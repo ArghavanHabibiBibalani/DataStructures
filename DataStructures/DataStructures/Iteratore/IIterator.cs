08b0a816929e5eafcb78605cfd4d860bde5fbe10
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-19 14:42:50 +0330

    IIterator interface
    not completed

---
+++ DataStructures/DataStructures/Iteratore/IIterator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Iteratore
+{
+    internal interface IIterator<E> : IEnumerator<E>
+    {
+        public bool HasNext();
+        public E Next();
+        public void Remove();
+    }
+}
