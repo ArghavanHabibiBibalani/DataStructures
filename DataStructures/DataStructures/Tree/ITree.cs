61aa78e2ea1bd0b5f07b3a30c12c6914f3627b05
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-19 14:43:10 +0330

    ITree interface.

---
+++ DataStructures/DataStructures/Tree/ITree.cs
@@ -0,0 +1,26 @@
+﻿using DataStructures.Lists.PositionalLists;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tree
+{
+    internal interface ITree<E> : IEnumerable<E>
+    {
+        IPosition<E> Root { get; set; }
+        IPosition<E> Parent(IPosition<E> ppsition);
+        IEnumerable<IPosition<E>> Children(IPosition<E> position);
+        int Numchildren(IPosition<E> position);
+        bool IsInternal(IPosition<E> position);
+        bool IsExternal(IPosition<E> position);
+        bool IsRoot(IPosition<E> position);
+        bool IsLeaf(IPosition<E> position);
+        int Size();
+        bool IsEmpty();
+        IEnumerator<E> Enumerator();
+        IEnumerable<IPosition<E>> Positions();
+
+    }
+}
