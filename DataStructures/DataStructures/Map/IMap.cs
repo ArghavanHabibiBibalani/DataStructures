db07f55571e6fef4b52e2a9c2e643a7d7a54e463
arghavan <arghavan.habibibibalani@gmail.com>
2024-03-26 22:13:04 +0330

    add IMap

---
+++ DataStructures/DataStructures/Map/IMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Map
+{
+   public interface IMap<K, V>
+    {
+        int Size();
+        bool IsEmpty();
+        V Get(K key);
+        V Put(K key, V value);
+        V Remove(K key);
+        IEnumerable<K> KeySet();
+        IEnumerable<V> Values();
+        IEnumerable<KeyValuePair<K, V>> EntrySet();
+    }
+
+}
